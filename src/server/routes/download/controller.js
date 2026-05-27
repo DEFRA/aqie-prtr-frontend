ae95abe8dfa37597363befe0c25945a37803c187
Cora-Jane Meade <cora-jane.meade@cognizant.com>
2026-05-27 11:30:14 +0100

    Download links text and example href

--- src/server/routes/download/controller.js
+++ src/server/routes/download/controller.js
@@ -1,11 +1,20 @@
-import { downloadPageContent } from './content.js'
+import { downloadContent } from './content.js'
+import { mockData } from './mock-data.js'
 
 export const downloadController = {
   handler(request, h) {
     const { language = 'en' } = request.params // optional language parameter
-    const content = downloadPageContent[language]
+    const content = downloadContent[language]
+
+    const downloadLinks = [...mockData].reverse().map(item => ({
+      text: `${content.downloadPrefix} ${item.year} ${content.dataSuffix}`,
+      href: item.fileUrl
+    }))
 
     return h.view('download/index', {
+      pageTitle: content.pageTitle,
+      description: content.description,
+      downloadLinks
     })
   }
 }
