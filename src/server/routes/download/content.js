0e08def84f2fed94bbb051660c4a0388311c20c7
Cora-Jane Meade <cora-jane.meade@cognizant.com>
2026-06-22 13:35:40 +0100

    Changed page title to match prototype

--- src/server/routes/download/content.js
+++ src/server/routes/download/content.js
@@ -1,12 +1,12 @@
 export const downloadContent = {
   en: {
-    pageTitle: 'Download Data',
+    pageTitle: 'Download all data for a year',
     description: 'Some information on what the downloads will contain?',
     downloadPrefix: 'Download',
     dataSuffix: 'data'
   },
   cy: {
-    pageTitle: 'Download Data --CY',
+    pageTitle: 'Download all data for a year --CY',
     description: 'Some information on what the downloads will contain? --CY',
     downloadPrefix: 'Download --CY',
     dataSuffix: 'data --CY'
